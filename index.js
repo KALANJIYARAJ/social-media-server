74bae61be0307fbb390854f4a35fdc5cdacb6b8d
kalanjiyaraj01@gmail.com <kalanjiyaraj01@gmail.com>
2023-02-19 08:56:11 +0530

    storage and others added

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const router = express.Router();
+const path = require("path");
 
 dotenv.config();
 
@@ -22,6 +24,24 @@ mongoose.connect(
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/image");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
